84228cd5bd5bf52e482b10c9945bf9ffcf84d0c2
Phisanu Phoyoo <phisanuphoyoo2545@gmail.com>
2026-03-08 14:21:27 +0000

    Commit on "main"

--- data/foodMenu.js
+++ data/foodMenu.js
@@ -1,13 +1,13 @@
 const foodMenu = [
   {
-    name: "ข้าวผัดกุ้ง",
-    image: "/images/khao.jpg",
+    name: "ข้าวกะเพราหมูไข่ดาว",
+    image: "/images/77dbed27-b746-4e00-8399-96fad42b621d.jpg",
     nutrition: 
     {
-      calories: 520,
+      calories: 650,
       protein: "30g",
-      carbs: "60g",
-      fat: "15g"
+      carbs: "55g",
+      fat: "28g"
     }
   },
   {
@@ -22,16 +22,16 @@ const foodMenu = [
     }
   },
   {
-    name: "ข้าวกะเพราหมูไข่ดาว",
-    image: "/images/77dbed27-b746-4e00-8399-96fad42b621d.jpg",
+    name: "ข้าวผัดกุ้ง",
+    image: "/images/khao.jpg",
     nutrition: 
     {
-      calories: 650,
+      calories: 520,
       protein: "30g",
-      carbs: "55g",
-      fat: "28g"
+      carbs: "60g",
+      fat: "15g"
     }
-  }
+  },
 ];
 
 module.exports = foodMenu;
