c0307b57fdaf5f73673d85eeb0cac077da0c754c
Phisanu Phoyoo <phisanuphoyoo2545@gmail.com>
2026-03-13 08:43:54 +0000

    Update food menu with image and fix configurations

--- data/foodMenu.js
+++ data/foodMenu.js
@@ -18,6 +18,26 @@ const foodMenu = [
       carbs: "55g",
       fat: "50g"
     }
+  },
+    {
+    name: "ข้าวผัดเพราหมูสับไข่ดาว",
+    image: "/images/77dbed27-b746-4e00-8399-96fad42b621d.jpg",
+    nutrition: {
+      calories: 700,
+      protein: "40g",
+      carbs: "60g",
+      fat: "35g"
+    }
+  },
+    {
+    name: "ข้าวผัดกุ้ง",
+    image: "/images/photo.jpg",
+    nutrition: {
+      calories: 500,
+      protein: "30g",
+      carbs: "20g",
+      fat: "25g"
+    }
   }
 ];
   
