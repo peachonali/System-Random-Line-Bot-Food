96c0da547832cb2add5975ed33d12ed8c07b54cc
Phisanu Phoyoo <phisanuphoyoo2545@gmail.com>
2026-03-13 07:43:54 +0000

    Remove all old food menus and images, add new menu: ข้าวหมูทอด

--- data/foodMenu.js
+++ data/foodMenu.js
@@ -1,57 +1,12 @@
 const foodMenu = [
   {
-    name: "ข้าวขาหมู",
-    image: "/images/leg.jpeg",
-    nutrition: 
-    {
-      calories: 600,
-      protein: "35g",
-      carbs: "50g",
-      fat: "30g"
-    }
-  },
-  {
-    name: "ข้าวกะเพราหมูไข่ดาว",
-    image: "/images/77dbed27-b746-4e00-8399-96fad42b621d.jpg",
-    nutrition: 
-    {
-      calories: 650,
+    name: "ข้าวหมูทอด",
+    image: "",
+    nutrition: {
+      calories: 750,
       protein: "30g",
-      carbs: "55g",
-      fat: "28g"
-    }
-  },
-  {
-    name: "ผัดไทยกุ้งสด",
-    image: "/images/padthai.jpg",
-    nutrition: 
-    {
-      calories: 486,
-      protein: "21g",
       carbs: "65g",
-      fat: "12g"
-    }
-  },
-  {
-    name: "ข้าวผัดกุ้ง",
-    image: "/images/khao.jpg",
-    nutrition: 
-    {
-      calories: 520,
-      protein: "30g",
-      carbs: "60g",
-      fat: "15g"
-    }
-  },
-    {
-    name: "เล้งแซ่บ",
-    image: "/images/1565.jpg",
-    nutrition: 
-    {
-      calories: 520,
-      protein: "25g",
-      carbs: "100g",
-      fat: "25g"
+      fat: "35g"
     }
   }
 ];
